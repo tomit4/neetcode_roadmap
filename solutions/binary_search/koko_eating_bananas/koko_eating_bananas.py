018d29ba8ca0458a7b43695f824d01bea61d4b4a
tomit4 <mosssap@gmail.com>
2024-12-25 22:55:59 -0800

    :construction: Adjusted solution for easier reading

--- solutions/binary_search/koko_eating_bananas/koko_eating_bananas.py
+++ solutions/binary_search/koko_eating_bananas/koko_eating_bananas.py
@@ -2,32 +2,30 @@ import math
 from typing import List
 
 
+# NOTE: Not Neetcode's solution, took a hybrid approach with this one,
+# some of this is his, some of this is another solution found on Leetcode site
+# note that binary search *lower bound* is indicative of this solution
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # min number of bananas koko can eat in an hour
         left: int = 1
+        # max number of bananas koko can eat in an hour
         right: int = max(piles)
-        res = right
 
-        while left <= right:
-            k: int = (left + right) // 2
-            hours: int = 0
+        def hours_needed(k):
+            hours_needed = 0
             for pile in piles:
-                # accumulate all the hours that koko can
-                # consume the bananas at given rate of k
-                hours += math.ceil(pile / k)
+                hours_needed += math.ceil(pile / k)
+            return hours_needed
 
-            # if the accumulated hours is leq the passed required hours,
-            # then our result is updated to take the minimum found thus far
-            # and our right pointer points to an int one less than the middle (k)
-            if hours <= h:
-                res = min(res, k)
-                right = k - 1
-            # otherwise the accumulated hours is gt our passed required hours
-            # and we simply move the left pointer one int  one more than the middle (k)
+        while left < right:
+            k: int = (left + right) // 2
+            if hours_needed(k) <= h:
+                right = k
             else:
                 left = k + 1
 
-        return res
+        return left
 
 
 if __name__ == "__main__":

210c5faee49cc101dc1112a35868a75c4f852812
tomit4 <mosssap@gmail.com>
2024-12-28 00:44:42 -0800

    :construction: Modified solutions with more test cases

--- solutions/binary_search/search_in_rotated_sorted_array/search_in_rotated_sorted_array.py
+++ solutions/binary_search/search_in_rotated_sorted_array/search_in_rotated_sorted_array.py
@@ -32,5 +32,19 @@ class Solution:
 if __name__ == "__main__":
     solution = Solution()
     print(solution.search(nums=[4, 5, 6, 7, 0, 1, 2], target=0))
+    assert solution.search(nums=[4, 5, 6, 7, 0, 1, 2], target=0) == 4
+
     print(solution.search(nums=[4, 5, 6, 7, 0, 1, 2], target=3))
+    assert solution.search(nums=[4, 5, 6, 7, 0, 1, 2], target=3) == -1
+
     print(solution.search(nums=[1], target=0))
+    assert solution.search(nums=[1], target=0) == -1
+
+    print(solution.search(nums=[1], target=1))
+    assert solution.search(nums=[1], target=1) == 0
+
+    print(solution.search(nums=[1, 3], target=3))
+    assert solution.search(nums=[1, 3], target=3) == 1
+
+    print(solution.search(nums=[3, 1], target=1))
+    assert solution.search(nums=[3, 1], target=1) == 1

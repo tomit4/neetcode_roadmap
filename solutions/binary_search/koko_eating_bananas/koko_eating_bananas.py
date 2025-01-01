ed6f5c41a6771cb6502a343a65a0ac2eaac208c2
tomit4 <mosssap@gmail.com>
2025-01-01 06:26:55 -0800

    :construction: Another solution to ol' koko

--- solutions/binary_search/koko_eating_bananas/koko_eating_bananas.py
+++ solutions/binary_search/koko_eating_bananas/koko_eating_bananas.py
@@ -1,11 +1,29 @@
-import math
+from math import ceil
 from typing import List
+
+
+# Another solution found on math.stackexchange
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        min_speed = 1
+        max_speed = max(piles)
+
+        l, r = min_speed, max_speed
+        while l < r:
+            mid_speed = (l + r) // 2
+            hours_needed = sum(ceil(pile / mid_speed) for pile in piles)
+            if hours_needed <= h:
+                r = mid_speed
+            else:  # t > h
+                l = mid_speed + 1
+
+        return l
 
 
 # NOTE: Not Neetcode's solution, took a hybrid approach with this one,
 # some of this is his, some of this is another solution found on Leetcode site
 # note that binary search *lower bound* is indicative of this solution
-class Solution:
+class Solution_1:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         # min number of bananas koko can eat in an hour
         left: int = 1
@@ -15,7 +33,7 @@ class Solution:
         def hours_needed(k):
             hours_needed = 0
             for pile in piles:
-                hours_needed += math.ceil(pile / k)
+                hours_needed += ceil(pile / k)
             return hours_needed
 
         while left < right:
---
+++ solutions/binary_search/readme.md
@@ -0,0 +1,18 @@
+# Binary Search
+
+**Prerequisites:**
+
+- Search Array
+- Search Range
+
+**Problems:**
+
+| No. | Name                                 | Difficulty |
+| --- | ------------------------------------ | ---------- |
+| 1   | Binary Search                        | Easy       |
+| 2   | Search a 2D Matrix                   | Medium     |
+| 3   | Koko Eating Bananas                  | Medium     |
+| 4   | Find Minimum In Rotated Sorted Array | Medium     |
+| 5   | Search In Rotated Sorted Array       | Medium     |
+| 6   | Time Based Key Value Store           | Medium     |
+| 7   | Median of Two Sorted Arrays          | Hard       |

6a6e0759ef9cd7a11b8a3322f0885eceea35b6c9
tomit4 <mosssap@gmail.com>
2025-01-18 14:49:27 -0800

    :construction: Added asserts to solutions in arrays/hashing

--- solutions/arrays_hashing/contains_duplicate/contains_duplicate.py
+++ solutions/arrays_hashing/contains_duplicate/contains_duplicate.py
@@ -15,5 +15,8 @@ class Solution:
 if __name__ == "__main__":
     solution = Solution()
     print(solution.containsDuplicate(nums=[1, 2, 3, 1]))
+    assert solution.containsDuplicate(nums=[1, 2, 3, 1]) == True
     print(solution.containsDuplicate(nums=[1, 2, 3, 4]))
+    assert solution.containsDuplicate(nums=[1, 2, 3, 4]) == False
     print(solution.containsDuplicate(nums=[1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+    assert solution.containsDuplicate(nums=[1, 1, 1, 3, 3, 4, 3, 2, 4, 2]) == True
--- solutions/arrays_hashing/group_anagrams/group_anagrams.py
+++ solutions/arrays_hashing/group_anagrams/group_anagrams.py
@@ -2,6 +2,21 @@ from collections import defaultdict
 from typing import List
 
 
+class Solution_2:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = {}
+
+        for s in strs:
+            # causes time complexity to be (m * n * log(n)) where m is num of strings and n is avg num of characters in str
+            sorted_str = "".join(sorted(s))
+            if sorted_str not in res:
+                res[sorted_str] = []
+
+            res[sorted_str].append(s)
+
+        return list(res.values())
+
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)  # mapping charCount to list of Anagrams
@@ -23,5 +38,15 @@ class Solution:
 if __name__ == "__main__":
     solution = Solution()
     print(solution.groupAnagrams(strs=["eat", "tea", "tan", "ate", "nat", "bat"]))
+
+    assert solution.groupAnagrams(strs=["eat", "tea", "tan", "ate", "nat", "bat"]) == [
+        ["eat", "tea", "ate"],
+        ["tan", "nat"],
+        ["bat"],
+    ]
+
     print(solution.groupAnagrams(strs=[""]))
+    assert solution.groupAnagrams(strs=[""]) == [[""]]
+
     print(solution.groupAnagrams(strs=["a"]))
+    assert solution.groupAnagrams(strs=["a"]) == [["a"]]
